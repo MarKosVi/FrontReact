cd97d868ba1cad535af6957faa11ee5ee1544a92
markosvi <mfonsecabarros@gmail.com>
2020-06-26 11:43:07 -0300

    nav-user

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,11 +18,11 @@ export default function Login(){
     }
 
     return(
-      
+
       <Container className="container">
       <Container className="row">
-            <Container className="col-md-12 mb-4">
-              <Container className="card is-card-dark chart-card rounded p-4">
+            <Container className="col-md-12 mb-5">
+              <Container className="card w-50 mx-auto is-card-dark chart-card rounded pt-5">
               <img src={UserImg} className="rounded-circle mx-auto" alt="user" />
               <div className="Login">
                 <form onSubmit={handleSubmit}>
@@ -43,8 +43,8 @@ export default function Login(){
                       type="password"
                     />
                   </FormGroup>
-                  <Button className="btn btn-primary" block bsSize="large" disabled={!validateForm()} type="submit">
-                    Login
+                  <Button className="btn btn-primary font-weight-bold text-large " block bsSize="large" disabled={!validateForm()} type="submit">
+                    Entrar
                   </Button>
                 </form>
               </div>
--- src/components/Navs.js
+++ src/components/Navs.js
@@ -8,21 +8,26 @@ import UserImg from "../assets/images/user.png";
 
 function Navs(){
     return (
-        <Nav className="navbar fixed-top nav-primary is-dark is-light-text">
-          <Container className="text-large"><img src={logo} className="imgSize"  alt="logo" />Covid-19 
+      <Nav className="nav p-2 fixed-top nav-primary is-dark is-light-text">
+        <Container className="container">
+          <Container className="text-large">
+          <img src={logo} className="imgSize"  alt="logo" />Covid-19
           <span className="text-white pl-5 ml-md-5 pr-md-2"><Link to="/">Home</Link></span>
           <span className="text-white pl-4 pr-2"><Link to="/MapContainer">Mapa</Link></span>
           <span className="text-white pl-4 pr-2"><Link to="/Dashboard">Dashboard</Link></span>
           </Container>
-          <Container className="navbar-nav">
-          <Container className="user-detail-section">
-            <span className="pr-4">Olá, user</span>
-            <span className="img-container">
-              <img src={UserImg} className="rounded-circle" alt="user" />
-            </span>
+          <Container className="row justify-content-end use">
+            <Container className="navbar-nav m-4">
+              <Container className="user-detail-section">
+                <span className="pr-4">Olá, user</span>
+                <span className="img-container">
+                  <img src={UserImg} className="rounded-circle" alt="user" />
+                </span>
+              </Container>
+            </Container>
           </Container>
-          </Container>
-        </Nav>
+        </Container>
+      </Nav>
     );
 }
 
