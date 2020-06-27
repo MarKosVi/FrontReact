343ba3c6f9c33c3bcc3744cd4570bfd5e54569b3
Rodrigo Drumond de Paula <rodrigodru33@gmail.com>
2020-06-27 00:42:31 -0300

    Updates

---
+++ precache-manifest.fb3c103bfb1b61375c84bdeedc3ae9cd.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f4873531420ac379d55",
+    "url": "/online-retail-dashboard/static/css/main.27692403.chunk.css"
+  },
+  {
+    "revision": "1f4873531420ac379d55",
+    "url": "/online-retail-dashboard/static/js/main.1f487353.chunk.js"
+  },
+  {
+    "revision": "6567bbb1a59ede678f9b",
+    "url": "/online-retail-dashboard/static/css/1.7880953f.chunk.css"
+  },
+  {
+    "revision": "6567bbb1a59ede678f9b",
+    "url": "/online-retail-dashboard/static/js/1.6567bbb1.chunk.js"
+  },
+  {
+    "revision": "b9ff2395e44c38d7f6b0",
+    "url": "/online-retail-dashboard/static/js/runtime~main.b9ff2395.js"
+  },
+  {
+    "revision": "572c50c64ca36c37d95b23e790bb1119",
+    "url": "/online-retail-dashboard/static/media/logo.572c50c6.png"
+  },
+  {
+    "revision": "5df6cc928ef5d1dbe39ad66e84659f63",
+    "url": "/online-retail-dashboard/static/media/telaimg.5df6cc92.png"
+  },
+  {
+    "revision": "b1f988cadc1dfa4938729cf6505f3817",
+    "url": "/online-retail-dashboard/static/media/tela1img.b1f988ca.png"
+  },
+  {
+    "revision": "3c5090a13a35e52fa4519782c1c556e7",
+    "url": "/online-retail-dashboard/static/media/tela2img.3c5090a1.png"
+  },
+  {
+    "revision": "1f6e0bfa1f7043a72a65c498b0d4a0b4",
+    "url": "/online-retail-dashboard/static/media/tela3img.1f6e0bfa.png"
+  },
+  {
+    "revision": "5271abec1bf7e42f4ce5463807533d46",
+    "url": "/online-retail-dashboard/static/media/tela4img.5271abec.png"
+  },
+  {
+    "revision": "5c2a00a94d4cdbdb9cd2e002c0e4348b",
+    "url": "/online-retail-dashboard/static/media/tela5img.5c2a00a9.png"
+  },
+  {
+    "revision": "21e3699b557149af338712621dd2f901",
+    "url": "/online-retail-dashboard/static/media/oqueé.21e3699b.png"
+  },
+  {
+    "revision": "c99bd6412f04e1696b3d76a79884da9b",
+    "url": "/online-retail-dashboard/static/media/sintomas.c99bd641.png"
+  },
+  {
+    "revision": "48b440f61e7d214eaa559e079a13cac4",
+    "url": "/online-retail-dashboard/static/media/Comoproteger.48b440f6.png"
+  },
+  {
+    "revision": "348c4688ed3211b189c6d0ee0e9b1af4",
+    "url": "/online-retail-dashboard/static/media/Comotransmitido - Diagnóstico.348c4688.png"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/online-retail-dashboard/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "4fcdd97fadc3a1d9887f816f2aa67f1d",
+    "url": "/online-retail-dashboard/static/media/CircularStd-Medium.4fcdd97f.otf"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/online-retail-dashboard/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/online-retail-dashboard/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/online-retail-dashboard/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/online-retail-dashboard/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/online-retail-dashboard/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/online-retail-dashboard/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/online-retail-dashboard/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/online-retail-dashboard/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/online-retail-dashboard/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/online-retail-dashboard/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/online-retail-dashboard/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/online-retail-dashboard/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/online-retail-dashboard/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/online-retail-dashboard/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/online-retail-dashboard/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/online-retail-dashboard/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "08bcb608d8f3dc504fe52b273a71f870",
+    "url": "/online-retail-dashboard/index.html"
+  }
+];
